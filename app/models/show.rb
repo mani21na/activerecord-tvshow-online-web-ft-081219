06e7d2ffa93f651438afa1a1b2908dc603647d97
Hanna Lee <mani21na@gmail.com>
2019-10-27 06:09:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,29 @@
 class Show < ActiveRecord::Base
-  def Show::highest_rating
-    Show.maximum("rating")
-  end
-
-  def Show::most_popular_show
-    Show.order(rating: :desc).first
-  end
-
-  def Show::lowest_rating
-    Show.minimum("rating")
-  end
-
-  def Show::least_popular_show
-    Show.order(rating: :asc).first
-  end
-
-  def Show::ratings_sum
-    Show.sum("rating")
-  end
-
-  def Show::popular_shows
-    Show.where("rating > 5")
-  end
-
-  def Show::shows_by_alphabetical_order
-    Show.order(name: :asc)
-  end
+    def self.highest_rating
+      self.maximum(:rating)
+    end
+  
+    def self.most_popular_show
+      self.where("rating = ?", self.highest_rating).first
+    end
+  
+    def self.lowest_rating
+      self.minimum(:rating)
+    end
+  
+    def self.least_popular_show
+      self.where("rating = ?", self.lowest_rating).first
+    end
+  
+    def self.ratings_sum
+      self.sum(:rating)
+    end
+    
+    def self.popular_shows
+      self.where("rating >= ?", 5)
+    end
+  
+    def self.shows_by_alphabetical_order
+      self.order(:name)
+    end
 end
